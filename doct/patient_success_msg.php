e4bf7f0db2015f3be33ea19c0ffbf3bd0b6b8acc
Mounika105 <mounikou2018@gmail.com>
2020-05-05 17:59:22 +0530

    hi

--- doct/patient_success_msg.php
+++ doct/patient_success_msg.php
@@ -1,28 +1,139 @@
+<?php session_start();  ?>
 <?php include('header.php'); ?>
 
+<html>
+
+<head>
+	<title>Patient Login form</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+	<style>
+		html,
+		body {
+			display: flex;
+			justify-content: center;
+			height: 100%;
+		}
+
+		body,
+		div,
+		h1,
+		form,
+		input,
+		p {
+			padding: 0;
+			margin: 0;
+			outline: none;
+			font-family: Roboto, Arial, sans-serif;
+			font-size: 16px;
+			color: #666;
+		}
+
+		h1 {
+			padding: 10px 0;
+			font-size: 32px;
+			color: #4ADA75;
+			font-weight: 300;
+			text-align: center;
+		}
+
+		p {
+			font-size: 12px;
+		}
+
+		hr {
+			color: #a9a9a9;
+			opacity: 0.3;
+		}
+
+		.main-block {
+			max-width: 340px;
+			min-height: 360px;
+			padding: 10px 0;
+			margin: auto;
+			border-radius: 5px;
+			border: solid 1px #ccc;
+			box-shadow: 1px 2px 5px rgba(0, 0, 0, .31);
+			background: #ebebeb;
+		}
+
+		form {
+			margin: 0 30px;
+		}
+
+		label#icon {
+			margin: 0;
+			border-radius: 5px 0 0 5px;
+		}
 
 
+		input[type=text],
+		input[type=password] {
+			width: calc(100% - 57px);
+			height: 36px;
+			margin: 13px 0 0 -5px;
+			padding-left: 10px;
+			border-radius: 0 5px 5px 0;
+			border: solid 1px #cbc9c9;
+			box-shadow: 1px 2px 5px rgba(0, 0, 0, .09);
+			background: #fff;
+		}
 
+		input[type=password] {
+			margin-bottom: 15px;
+		}
 
-<!-- this is for donor registraton -->
-<div class="login" style="background-color:#fff;">
-	<h1 class="text-center" style="background-color:#4ADA75;color: #fff;">Congratulations....!!!</h1>
-	<p class="text-center">You have been registered!</p>
-	<div class="formstyle" style="float: right;padding:20px;border: 1px solid lightgrey;margin-right:415px; margin-bottom:30px;background-color:#f3f3f8;color:#141313;">
-		<form action="" method="post" class="text-center">
-			<label>
-				Email: <input type="email" name="email" placeholder="username" required>
-			</label><br><br>
-			<label>
-				Password: <input type="password" name="password" placeholder="password" required>
-			</label><br><br>
-			<button name="submit" type="submit" style="margin-left:36px;width: 85px;border-radius: 3px;">Login</button> <br>
+		#icon {
+			display: inline-block;
+			padding: 9.3px 15px;
+			box-shadow: 1px 2px 5px rgba(0, 0, 0, .09);
+			background: #4ADA75;
+			color: #fff;
+			text-align: center;
+		}
 
-			<!-- Not a member?<a href="donor_seeker_regi.php" title="create a account" target="" style="color:black;">&nbsp;Sign Up</a> <br> -->
+		.btn-block {
+			margin-top: 10px;
+			text-align: center;
+		}
+
+		button {
+			width: 100%;
+			padding: 10px 0;
+			margin: 10px auto;
+			border-radius: 5px;
+			border: none;
+			background: #4ADA75;
+			font-size: 14px;
+			font-weight: 600;
+			color: #fff;
+		}
+
+		button:hover {
+			background: #4ADA75;
+		}
+	</style>
+</head>
+
+<body>
+	<div class="main-block">
+		<form action="" method="post">
+<h3>Registration successful</h3>
+			<hr>
+			<label id="icon" for="name"><i class="fas fa-envelope"></i></label>
+			<input type="email" name="email" id="name" placeholder="Email" required />
+			<label id="icon" for="name"><i class="fas fa-unlock-alt"></i></label>
+			<input type="password" name="password" id="name" placeholder="Password" required />
+			<hr>
+			<div class="btn-block">
+				<button type="submit" name="submit">submit</button>
+			</div>
+			<span style="color:#0000FF;">Not a member yet?</span> <a href="patient_regi.php" title="create a account" target="" style="color:#000;">&nbsp;Sign Up</a> <br>
 
 
 			<!-- login validation -->
 			<?php
+			$_SESSION['patient'] = "";
 
 			include('config.php');
 			if (isset($_POST["submit"])) {
@@ -33,12 +144,13 @@
 				$result = $conn->query($sql);
 
 				if ($result->num_rows > 0) {
-					// $_SESSION["email"]= $_POST["email"];
-					// $_SESSION['userstatus']= "yes";
+					$_SESSION["email"] = $_POST["email"];
+
+					$_SESSION['patient'] = "yes";
 					echo "<script>location.replace('patient/dashboard.php');</script>";
 					// echo "u are supposed to redirect to ur profile";
 				} else {
-					echo "<span style='color:red;'>Please, check  username and password</span>";
+					echo "<span style='color:red;'>Invalid username or password</span>";
 				}
 				$conn->close();
 			}
@@ -48,35 +160,8 @@
 
 
 		</form> <br>&nbsp;&nbsp;&nbsp;
-
-		<br>
-
-
-
-
-
-
 	</div>
-
-
-</div>
-
-
-
-
-<?php include('footer.php'); ?>
-
-
-
-</div><!--  containerFluid Ends -->
-
-
-
-
-<script src="js/bootstrap.min.js"></script>
-
-
-
+	<script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
